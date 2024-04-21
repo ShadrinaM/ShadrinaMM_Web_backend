9f1328b28a32cf1a9d07c65a14836685cb04952a
BusinaLapusina <busina_lapusina@mail.ru>
2024-04-21 22:41:04 +0300

    ghfgh

--- Quest_4/form.php
+++ Quest_4/form.php
@@ -4,117 +4,212 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Form Drupal Coder</title>
+  <title>Form4</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+
+  <?php
+  if (!empty($messages)) {
+    print ('<div id="messages">');
+    // Выводим все сообщения.
+    foreach ($messages as $message) {
+      print ($message);
+    }
+    print ('</div>');
+  }
+  ?>
+
   <div class="fomaa">
     <form action="index.php" method="POST">
+
       <h3 id="forma">
         ФОРМА ДЛЯ ПОДПИСКИ НА РАССЫЛКУ С ЦИТАТИМИ ПРЕПОДАВАТЕЛЕЙ ФКТИПМ
       </h3>
+
       <label>
         <strong>
           Фамилия имя отчество:
         </strong>
-        <input name="fio" type="text" placeholder="ФИО" />
+        <input name="fio" type="text" <?php if ($errors['fio']) {
+          print 'class="error"';
+        } ?>
+          value="<?php print $values['fio']; ?>" placeholder="ФИО" />
       </label>
       <br>
+
+
       <label>
         <strong>
           Номер телефона:
         </strong>
-        <input name="phone" type="tel" />
+        <input name="phone" type="tel" <?php if ($errors['phone']) {
+          print 'class="error"';
+        } ?>
+          value="<?php print $values['phone']; ?>" />
       </label>
       <br>
+
+
       <label>
         <strong>
           Введите вашу почту:
         </strong>
-        <input name="mail" type="email" placeholder="email" />
+        <input name="mail" type="email" <?php if ($errors['mail']) {
+          print 'class="error"';
+        } ?>
+          value="<?php print $values['mail']; ?>" placeholder="email" />
       </label>
       <br>
+
       <strong>
         Укажите год рождения:
       </strong>
-      <select name="year">
+      <select name="year" <?php if ($errors['year']) {
+        print 'class="error"';
+      } ?>>
         <?php
         for ($i = 1922; $i <= 2022; $i++) {
-          printf('<option value="%d">%d год</option>', $i, $i);
+          echo "<option value=\"$i\" ";
+          if (in_array($i, $values['year'])) {
+            print 'selected';
+          }
+          echo "$i год</option>";
         }
         ?>
-      </select><br />
+      </select>
+      <br />
+
+
       <strong>
         Укажите месяц рождения:
       </strong>
-      <select name="month">
+      <select name="month" <?php if ($errors['month']) {
+        print 'class="error"';
+      } ?>>
         <?php
         for ($i = 1; $i <= 12; $i++) {
-          printf('<option value="%d">%d месяц</option>', $i, $i);
+          echo "<option value=\"$i\" ";
+          if (in_array($i, $values['month'])) {
+            print 'selected';
+          }
+          echo "$i месяц</option>";
         }
         ?>
-      </select><br />
+      </select>
+      <br />
+
       <strong>
         Укажите день рождения:
       </strong>
-      <select name="day">
+      <select name="day" <?php if ($errors['day']) {
+        print 'class="error"';
+      } ?>>
         <?php
         for ($i = 1; $i <= 31; $i++) {
-          printf('<option value="%d">%d день</option>', $i, $i);
+          echo "<option value=\"$i\" ";
+          if (in_array($i, $values['month'])) {
+            print 'selected';
+          }
+          echo "$i день</option>";
         }
         ?>
       </select>
       <br>
+
+
       <br>
       <strong>
         Пол:
       </strong>
       <label>
-        <input type="radio" name="pol" required value="1">
+        <input type="radio" name="pol" required value="1" <?php if ($values['pol'] === '1') {
+          print 'checked';
+        } ?>>
         Мужской
       </label>
       <label>
-        <input type="radio" name="pol" required value="2">
+        <input type="radio" name="pol" required value="2" <?php if ($values['pol'] === '2') {
+          print 'checked';
+        } ?>>
         Женский
       </label>
       <label>
-        <input type="radio" name="pol" required value="3">
+        <input type="radio" name="pol" required value="3" <?php if ($values['pol'] === '3') {
+          print 'checked';
+        } ?>>
         Ламинат
       </label>
       <br>
+
+
       <strong>
         Любимый язык программирования:
       </strong>
       <div class="language">
         <label>
-          <select name="langg[]" multiple="multiple">
-            <option value="1">С</option>
-            <option value="2">Pascal</option>
-            <option value="3">Scala</option>
-            <option value="4">C++</option>
-            <option value="5">Java</option>
-            <option value="6">Python</option>
-            <option value="7">JavaScript</option>
-            <option value="8">PHP</option>
-            <option value="9">Hascel</option>
-            <option value="10">Clojure</option>
-            <option value="11">Prolog</option>
+          <select name="langg[]" multiple="multiple" <?php if ($errors['langg']) {
+            print 'class="error"';
+          } ?>>
+            <option value="1" <?php if (in_array('1', $values['langg'])) {
+              print 'selected';
+            } ?>> С </option>
+            <option value="2" <?php if (in_array('2', $values['langg'])) {
+              print 'selected';
+            } ?>> Pascal </option>
+            <option value="3" <?php if (in_array('3', $values['langg'])) {
+              print 'selected';
+            } ?>> Scala </option>
+            <option value="4" <?php if (in_array('4', $values['langg'])) {
+              print 'selected';
+            } ?>> C++ </option>
+            <option value="5" <?php if (in_array('5', $values['langg'])) {
+              print 'selected';
+            } ?>> Java </option>
+            <option value="6" <?php if (in_array('6', $values['langg'])) {
+              print 'selected';
+            } ?>> Python </option>
+            <option value="7" <?php if (in_array('7', $values['langg'])) {
+              print 'selected';
+            } ?>> JavaScript </option>
+            <option value="8" <?php if (in_array('8', $values['langg'])) {
+              print 'selected';
+            } ?>> PHP </option>
+            <option value="9" <?php if (in_array('9', $values['langg'])) {
+              print 'selected';
+            } ?>> Hascel </option>
+            <option value="10" <?php if (in_array('10', $values['langg'])) {
+              print 'selected';
+            } ?>> Clojure </option>
+            <option value="11" <?php if (in_array('11', $values['langg'])) {
+              print 'selected';
+            } ?>> Prolog </option>
           </select>
         </label>
       </div>
+
       <label>
         <strong>
           Биография:
         </strong>
-        <textarea name="biog" placeholder="Я ждал 12 лет в Азкабане..">
-                </textarea>
+        <textarea name="biog" <?php if ($errors['biog']) {
+          print 'class="error"';
+        } ?>
+          value="<?php print $values['biog']; ?>" placeholder="Я ждал 12 лет в Азкабане..">
+        </textarea>
       </label>
+
       <label>
-        <input type="checkbox" name="V" />
+        <input type="checkbox" name="V" <?php if ($errors['V']) {
+          print 'class="error"';
+        } ?>
+          value="<?php print $values['V']; ?>" />
         c контрактом ознакомлен(а)
       </label>
+
       <input type="submit" value="Сохранить" />
+
     </form>
   </div>
 </body>
--- Quest_4/index.php
+++ Quest_4/index.php
@@ -1,131 +1,261 @@
 <?php
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
 
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  $messages = array();
   // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-  if (!empty($_GET['save'])) {
-    // Если есть параметр save, то выводим сообщение пользователю.
-    print ('Спасибо, результаты сохранены.');
+
+
+  if (!empty($_COOKIE['save'])) {
+    setcookie('save', '', 100000); // Удаляем куку, указывая время устаревания в прошлом.
+    $messages[] = 'Спасибо, результаты сохранены.'; // Если есть параметр save, то выводим сообщение пользователю.
   }
-  // Включаем содержимое файла form.php.
+
+
+  //fio phone mail year month day pol langg biog V
+  $errors = array();
+  $errors['fio'] = !empty($_COOKIE['fio_error']);
+  $errors['phone'] = !empty($_COOKIE['phone_error']);
+  $errors['mail'] = !empty($_COOKIE['mail_error']);
+  $errors['year'] = !empty($_COOKIE['year_error']);
+  $errors['month'] = !empty($_COOKIE['month_error']);
+  $errors['day'] = !empty($_COOKIE['day_error']);
+  $errors['pol'] = !empty($_COOKIE['pol_error']);
+  $errors['langg'] = !empty($_COOKIE['langg_error']);
+  $errors['biog'] = !empty($_COOKIE['biog_error']);
+  $errors['V'] = !empty($_COOKIE['V_error']);
+
+
+
+  if ($errors['fio']) {
+    setcookie('fio_error', '', 100000);
+    setcookie('fio_value', '', 100000);
+    $messages[] = '<div class="error">Заполните имя.</div>';
+  }
+  if ($errors['phone']) {
+    setcookie('phone_error', '', 100000);
+    setcookie('phone_value', '', 100000);
+    $messages[] = '<div class="error">Заполните телефон.</div>';
+  }
+  if ($errors['mail']) {
+    setcookie('mail_error', '', 100000);
+    setcookie('mail_value', '', 100000);
+    $messages[] = '<div class="error">Заполните почту.</div>';
+  }
+  if ($errors['year']) {
+    setcookie('year_error', '', 100000);
+    setcookie('year_value', '', 100000);
+    $messages[] = '<div class="error">Выберите год рождения.</div>';
+  }
+  if ($errors['month']) {
+    setcookie('month_error', '', 100000);
+    setcookie('month_value', '', 100000);
+    $messages[] = '<div class="error">Выберите месяц рождения.</div>';
+  }
+  if ($errors['day']) {
+    setcookie('day_error', '', 100000);
+    setcookie('day_value', '', 100000);
+    $messages[] = '<div class="error">Выберите день рождения.</div>';
+  }
+  if ($errors['pol']) {
+    setcookie('pol_error', '', 100000);
+    setcookie('pol_value', '', 100000);
+    $messages[] = '<div class="error">Выберите пол.</div>';
+  }
+  if ($errors['langg']) {
+    setcookie('langg_error', '', 100000);
+    setcookie('langg_value', '', 100000);
+    $messages[] = '<div class="error">Что-то не так с языком программирования!.</div>';
+  }
+  if ($errors['biog']) {
+    setcookie('biog_error', '', 100000);
+    setcookie('biog_value', '', 100000);
+    $messages[] = '<div class="error">Заполните биографию.</div>';
+  }
+  if ($errors['V']) {
+    setcookie('V_error', '', 100000);
+    setcookie('V_value', '', 100000);
+    $messages[] = '<div class="error">Подтвердите согласие.</div>';
+  }
+
+
+  //fio phone mail year month day pol langg biog V
+  $values = array(); // Складываем значения полей в массив.
+  $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
+  $values['phone'] = empty($_COOKIE['phone_value']) ? '' : $_COOKIE['phone_value'];
+  $values['mail'] = empty($_COOKIE['mail_value']) ? '' : $_COOKIE['mail_value'];
+  $values['year'] = empty($_COOKIE['year_value']) ? '' : $_COOKIE['year_value'];
+  $values['month'] = empty($_COOKIE['month_value']) ? '' : $_COOKIE['month_value'];
+  $values['day'] = empty($_COOKIE['day_value']) ? '' : $_COOKIE['day_value'];
+  $values['pol'] = empty($_COOKIE['pol_value']) ? '' : $_COOKIE['pol_value'];
+  $values['langg'] = empty($_COOKIE['langg_value']) ? array() : unserialize($_COOKIE['langg_value']);
+  $values['biog'] = empty($_COOKIE['biog_value']) ? '' : $_COOKIE['biog_value'];
+  $values['V'] = empty($_COOKIE['V_value']) ? '' : $_COOKIE['V_value'];
+
+
+
+
   include ('form.php');
-  // Завершаем работу скрипта.
   exit();
-}
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-$errors = FALSE;
-if (empty($_POST['fio']) || !preg_match('/^[а-яА-ЯёЁa-zA-Z\s-]{1,150}$/u', $_POST['fio'])) {
-  print ('Заполните имя.<br/>');
-  $errors = TRUE;
-}
-if (empty($_POST['phone']) || !preg_match('/^\+[0-9]{11}$/', $_POST['phone'])) {
-  print ('Заполните телефон.<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['mail']) || !preg_match('/^([a-z0-9_-]+(?:[-_.]?[a-z0-9]+)?@[a-z0-9_.-]+(?:\.?[a-z0-9]+)?\.[a-z]{2,5})$/i', $_POST['mail'])) {
-  print ('Заполните почту.<br/>');
-  $errors = TRUE;
-}
-if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
-  print ('Заполните год.<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['month']) || !is_numeric($_POST['year']) || $_POST['month'] < 0 || $_POST['month'] > 12) {
-  print ('Заполните месяц.<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['day']) || !is_numeric($_POST['year']) || $_POST['day'] < 0 || $_POST['day'] > 31) {
-  print ('Заполните день.<br/>');
-  $errors = TRUE;
-}
+  include ('../Secret.php');
+  $user = userr;
+  $pass = passs;
+  $db = new PDO(
+    "mysql:host=localhost;dbname=$user",
+    $user,
+    $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+  );
 
 
-$polCheck = $_POST['pol'] == "1" || $_POST['pol'] == "2" || $_POST['pol'] == "3";
-if (empty($_POST['pol']) || !$polCheck) {
-  $errors = TRUE;
-}
+  $errors = FALSE;
+  if (empty($_POST['fio']) || !preg_match('/^[а-яА-ЯёЁa-zA-Z\s-]{1,150}$/u', $_POST['fio'])) {
+    setcookie('fio_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60);
+  }
 
 
-include ('../Secret.php');
-$user = userr;
-$pass = passs;
-$db = new PDO(
-  "mysql:host=localhost;dbname=$user",
-  $user,
-  $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
+  if (empty($_POST['phone']) || !preg_match('/^\+[0-9]{11}$/', $_POST['phone'])) {
+    setcookie('phone_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('phone_value', $_POST['phone'], time() + 30 * 24 * 60 * 60);
+  }
 
-if (empty($_POST['langg'])) {
-  print ('Выберите язык программирования.<br/>');
-  $errors = TRUE;
-} else {
-  $sth = $db->prepare("SELECT id FROM Lang");
-  $sth->execute();
-  $langs = $sth->fetchAll();
-  foreach ($_POST['langg'] as $lang) {
-    $flag = TRue;
-    foreach ($langs as $index)
-      if ($index[0] == $lang) {
-        $flag = false;
+
+  if (empty($_POST['mail']) || !preg_match('/^([a-z0-9_-]+(?:[-_.]?[a-z0-9]+)?@[a-z0-9_.-]+(?:\.?[a-z0-9]+)?\.[a-z]{2,5})$/i', $_POST['mail'])) {
+    setcookie('mail_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('mail_value', $_POST['mail'], time() + 30 * 24 * 60 * 60);
+  }
+
+
+  if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
+    setcookie('year_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('year_value', $_POST['year'], time() + 30 * 24 * 60 * 60);
+  }
+
+
+  if (empty($_POST['month']) || !is_numeric($_POST['month']) || $_POST['month'] < 0 || $_POST['month'] > 12) {
+    setcookie('month_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('month_value', $_POST['month'], time() + 30 * 24 * 60 * 60);
+  }
+
+
+  if (empty($_POST['day']) || !is_numeric($_POST['day']) || $_POST['day'] < 0 || $_POST['day'] > 31) {
+    setcookie('day_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('day_value', $_POST['day'], time() + 30 * 24 * 60 * 60);
+  }
+
+
+  $polCheck = $_POST['pol'] == "1" || $_POST['pol'] == "2" || $_POST['pol'] == "3";
+  if (empty($_POST['pol']) || !$polCheck) {
+    setcookie('pol_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('pol_value', $_POST['pol'], time() + 30 * 24 * 60 * 60);
+  }
+
+
+  if (empty($_POST['langg'])) {
+    setcookie('langg_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    $sth = $db->prepare("SELECT id FROM Lang");
+    $sth->execute();
+    $langs = $sth->fetchAll();
+    $has_incorrect_lang = false;
+    foreach ($_POST['langg'] as $lang) {
+      $flag = true;
+      foreach ($langs as $index)
+        if ($index[0] == $lang) {
+          $flag = false;
+          break;
+        }
+      if ($flag == true) {
+        $has_incorrect_lang = true;
+        $errors = true;
         break;
       }
-    if ($flag == true) {
-      print ('Error: no valid language.<br/>');
-      $errors = true;
-      break;
+    }
+    if (!$has_incorrect_lang) {
+      setcookie('langg_value', serialize($_POST['langg']), time() + 30 * 24 * 60 * 60);
     }
   }
-}
 
 
-if (empty($_POST['biog'])) {
-  print ('Заполните биографию.<br/>');
-  $errors = TRUE;
-}
+  if (empty($_POST['biog'])) {
+    setcookie('biog_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('biog_value', $_POST['biog'], time() + 30 * 24 * 60 * 60);
+  }
 
-if (empty($_POST['V'])) {
-  print ('Подтвердите согласие.<br/>');
-  $errors = TRUE;
-}
 
-if ($errors) {
-  exit();
-}
+  if (empty($_POST['V'])) {
+    setcookie('V_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } else {
+    setcookie('V_value', $_POST['V'], time() + 30 * 24 * 60 * 60);
+  }
 
-$stmt = $db->prepare("INSERT INTO Person (fio, phone, mail, bornday, pol, biog, V) VALUES (:fio, :phone, :mail, :bornday, :pol, :biog, :V)"); //создание запроса
-$stmt->bindParam(':bornday', $bornday);
-$fio = $_POST['fio'];
-$phone = $_POST['phone'];
-$mail = $_POST['mail'];
-$bornday = $_POST['day'] . '.' . $_POST['month'] . '.' . $_POST['year'];
-$pol = $_POST['pol'];
-$biog = $_POST['biog'];
-$V = true;
-$stmt->bindParam(':fio', $fio);
-$stmt->bindParam(':phone', $phone);
-$stmt->bindParam(':mail', $mail);
 
-$stmt->bindparam(':pol', $pol);
-$stmt->bindparam(':biog', $biog);
-$stmt->bindparam(':V', $V);
-$stmt->execute(); //отправка
-$id = $db->lastInsertId();
+  if ($errors) {
+    header('Location: index.php');
+    exit();
+  } else {
+    setcookie('fio_error', '', -10000);
+    setcookie('phone_error', '', -10000);
+    setcookie('mail_error', '', -10000);
+    setcookie('year_error', '', -10000);
+    setcookie('month_error', '', -10000);
+    setcookie('day_error', '', -10000);
+    setcookie('pol_error', '', -10000);
+    setcookie('langg_error', '', -10000);
+    setcookie('biog_error', '', -10000);
+    setcookie('V_error', '', -10000);
+  }
 
-foreach ($_POST['langg'] as $lang) {
-  $stmt = $db->prepare("INSERT INTO person_and_lang (id_u, id_l) VALUES (:id_u,:id_l)");
-  $stmt->bindParam(':id_u', $id_u);
-  $stmt->bindParam(':id_l', $lang);
-  $id_u = $id;
+
+  $stmt = $db->prepare("INSERT INTO Person (fio, phone, mail, bornday, pol, biog, V) VALUES (:fio, :phone, :mail, :bornday, :pol, :biog, :V)"); //создание запроса
+  $stmt->bindParam(':bornday', $bornday);
+  $fio = $_POST['fio'];
+  $phone = $_POST['phone'];
+  $mail = $_POST['mail'];
+  $bornday = $_POST['day'] . '.' . $_POST['month'] . '.' . $_POST['year'];
+  $pol = $_POST['pol'];
+  $biog = $_POST['biog'];
+  $V = true;
+  $stmt->bindParam(':fio', $fio);
+  $stmt->bindParam(':phone', $phone);
+  $stmt->bindParam(':mail', $mail);
+
+  $stmt->bindparam(':pol', $pol);
+  $stmt->bindparam(':biog', $biog);
+  $stmt->bindparam(':V', $V);
   $stmt->execute(); //отправка
-}
+  $id = $db->lastInsertId();
 
-header('Location: ?save=1');
+  foreach ($_POST['langg'] as $lang) {
+    $stmt = $db->prepare("INSERT INTO person_and_lang (id_u, id_l) VALUES (:id_u,:id_l)");
+    $stmt->bindParam(':id_u', $id_u);
+    $stmt->bindParam(':id_l', $lang);
+    $id_u = $id;
+    $stmt->execute(); //отправка
+  }
+  setcookie('save', '1');
+  header('Location: ?save=1');
+
+}
+?>
